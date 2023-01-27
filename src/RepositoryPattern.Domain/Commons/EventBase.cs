8cbe1b0c83a9fd8487ff28e7affe5ef8c3944277
Mert Balasar <79214655+mertbalasar@users.noreply.github.com>
2023-01-27 15:45:11 +0300

    created domain layer

---
+++ src/RepositoryPattern.Domain/Commons/EventBase.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Domain.Commons
+{
+    public abstract class EventBase : INotification
+    {
+    }
+}
---
+++ src/RepositoryPattern.Domain/Entities/Users.cs
@@ -0,0 +1,17 @@
+﻿using RepositoryPattern.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Domain.Entities
+{
+    public class Users : EntityBase
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+    }
+}
